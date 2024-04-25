85f35d93faab7fceec3b32ebff3e8436a25df6d7
RenanGroh <renangroh@gmail.com>
2024-04-25 16:32:27 -0300

    start

---
+++ src/test/java/com/dev/api/adacoinapi/AdaCoinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.api.adacoinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaCoinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
