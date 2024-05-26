55496a1cea30c9e3cffb100ebe61340a197c23b0
RenanGroh <renangroh@gmail.com>
2024-05-26 20:03:44 -0300

    adicionado test do addFavoriteCurrency do UserService

--- src/test/java/com/dev/api/service/UserServiceTest.java
+++ src/test/java/com/dev/api/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.dev.api.service;
 
+import com.dev.api.adacoinapi.model.CurrencyQuote;
 import com.dev.api.adacoinapi.model.User;
+import com.dev.api.adacoinapi.repository.CurrencyQuoteRepository;
 import com.dev.api.adacoinapi.repository.UserRepository;
 import com.dev.api.adacoinapi.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,13 +11,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
+
+    @Mock
+    private CurrencyQuoteRepository quoteRepository;
 
     @Mock
     private UserRepository userRepository;
@@ -50,5 +59,31 @@ class UserServiceTest {
         User createdUser = userService.createUser(user);
 
         assertEquals(expectedUser, createdUser);
+    }
+
+    @Test
+    void testAddFavoriteCurrency() {
+        Long userId = 1L;
+        Long quoteId = 1L;
+
+        User mockUser = new User();
+        mockUser.setId(userId);
+        mockUser.setFavoriteCurrencies(new ArrayList<>()); // Use ArrayList instead of HashSet
+
+        CurrencyQuote mockQuote = new CurrencyQuote();
+        mockQuote.setId(quoteId);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
+        when(quoteRepository.findById(quoteId)).thenReturn(Optional.of(mockQuote));
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        User updatedUser = userService.addFavoriteCurrency(userId, quoteId);
+
+        assertEquals(userId, updatedUser.getId());
+        assertTrue(updatedUser.getFavoriteCurrencies().contains(mockQuote));
+
+        verify(userRepository, times(1)).findById(userId);
+        verify(quoteRepository, times(1)).findById(quoteId);
+        verify(userRepository, times(1)).save(mockUser);
     }
 }
